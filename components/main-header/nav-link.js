4cd4355e51834ac4cc72b5dad20bdac38f4d4457
Maheer Akbar <maheerakbar@gmail.com>
2025-09-24 13:24:47 +0500

    updates

--- components/main-header/nav-link.js
+++ components/main-header/nav-link.js
@@ -1,17 +1,22 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import classes from './nav-link.module.css';
 
-export default function NavLink({ href, children}) {
-    const path = usePathname();
+export default function NavLink({ href, children }) {
+  const path = usePathname();
 
   return (
-    
-      <Link href={href} className={ path.startsWith(href) ? `${classes.active} ${classes.link}` : classes.link}>
-        {children}
-      </Link>
-    
+    <Link
+      href={href}
+      className={
+        path.startsWith(href)
+          ? `${classes.link} ${classes.active}`
+          : classes.link
+      }
+    >
+      {children}
+    </Link>
   );
 }
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -1,6 +1,7 @@
-import MealItem from "./meal-item";
-import classes from "./meal-grid.module.css";
-export default function MealGrid({ meals }) {
+import MealItem from './meal-item';
+import classes from './meals-grid.module.css';
+
+export default function MealsGrid({ meals }) {
   return (
     <ul className={classes.meals}>
       {meals.map((meal) => (
